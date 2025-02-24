73791c4950ef14ba40018ca26dc5829cde12f6e7
flores-97 <floresvictor15@gmail.com>
2025-02-24 07:52:47 -0800

    program menu updated with choices

--- COMP003A.FinalProject/Program.cs
+++ COMP003A.FinalProject/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace COMP003A.FinalProject
+﻿// Author: Victor Flores
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Zoo management system demonstrating inheritance, abstraction, polymorphism, and method overloading in C#
+using System.Xml.Linq;
+using COMP003A.FinalProject;
+using Microsoft.VisualBasic;
+
+namespace COMP003A.ZooManagementSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            List<Lego> legos = new List<Lego>();
+
+            Console.WriteLine("Welcome to the awesome Lego Inventory Management System!");
+
+            int choice;
+
+            while (true)
+            {
+                Console.WriteLine("\nInventory Management System Menu: \n");
+                Console.WriteLine("1. Add a Lego Set");
+                Console.WriteLine("2. Add a Lego Minifigure");
+                Console.WriteLine("3. Remove a Lego");
+                Console.WriteLine("4. Display All Lego");
+                Console.WriteLine("5. Exit");
+
+                Console.Write("Enter your choice: ");
+
+                choice = int.Parse(Console.ReadLine());
+
+
+
+                if (choice == 5) break;
+
+                else if (choice == 1)
+                {
+
+                }
+
+                else if (choice == 2)
+                {
+
+                }
+
+
+                else if (choice == 3)
+                {
+
+                }
+                else if (choice == 4)
+                {
+
+                }
+
+                else
+                {
+                    Console.WriteLine("\nInvalid entry. Select an option on the menu.\n");
+                }
+            }
+
+        }//ends program
+
+
     }
 }
